f0345ae981af57cecc75aac87a06734da0653a6f
Oscar Gutierrez <oscarfgutierrezo@gmail.com>
2022-04-17 20:09:08 -0500

    Add: team-01 to team-05 sections

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        "dark-violet": "#1E0E62",
+        "dark-violet-2": "#151439",
+        "light-violet": "#482BE7",
+        "green": "#25DAC5",
+        "primary": "#2F1893",
+        "red": "#EA3223",
+        "pink": "#E93A7D",
+        "blue": "#1DA1F2",
+        "grey": "#EBEAED",
+      },
+      fontFamily: {
+        "sans": "DM Sans"
+      },
+      backgroundImage: {
+        team02: 'url("../img/team-02a.png")',
+      },
+    },
+  },
+  plugins: [
+  ],
+}
